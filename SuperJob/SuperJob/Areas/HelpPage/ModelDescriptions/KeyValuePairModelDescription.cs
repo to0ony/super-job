27cf599f395816cafddfdbe4255442ac94b8eb83
to0ony <antonijomaric1@gmail.com>
2024-03-16 23:56:01 +0100

    Initialize project with multilayer architecture

---
+++ SuperJob/SuperJob/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace SuperJob.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ SuperJob/SuperJob/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperJob.WebApiApplication" Language="C#" %>
